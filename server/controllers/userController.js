03b7be50c311dddb736f968eae9b95bba3b2ff3a
aashii30 <aashi.sri30@gmail.com>
2025-06-23 00:22:45 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -49,7 +49,7 @@ export const login= async(req,res) => {
 
          const token = generateToken(userData._id)
 
-        res.json({success: true, userData: token, message: "Login successful"})
+        res.json({success: true, userData: userData, token, message: "Login successful"})
     } catch (error) {
         console.log(error.message);
         res.json({success: false, message: error.message})
